f5cd2fd09a8e876e8f6f149c9c7e1151c04fe254
Wasylews <ua.wasylews@gmail.com>
2016-05-09 18:16:46 +0300

    base program structure

--- main.asm
+++ main.asm
@@ -36,27 +36,75 @@ main proc c
 @@open_success:
     mov handle, ax
 
+@@main_loop:
     call show_main_menu
 
     ; switch al
     cmp al, 1               ; case 1
     je @@show_data
 
-    ; default(7)
-    jmp @@exit
+    cmp al, 2               ; case 2
+    je @@add_data
+
+    cmp al, 3               ; case 3
+    je @@remove_data
+
+    cmp al, 4               ; case 4
+    je @@edit_data
+
+    cmp al, 5
+    je @@search_data        ; case 5
+
+    cmp al, 6               ; case 6
+    je @@sort_data
+
+    cmp al, 7
+    je @@exit
 
 @@show_data:
     push handle
-    call db_show_table
+    call show_table
+    add sp, 2
+    jmp @@break
+
+@@add_data:
+    push handle
+    call add_record
+    add sp, 2
+    jmp @@break
+
+@@remove_data:
+    push handle
+    call remove_record
+    add sp, 2
+    jmp @@break
+
+@@edit_data:
+    push handle
+    call edit_record
+    add sp, 2
+    jmp @@break
+
+@@search_data:
+    push handle
+    call search_record
+    add sp, 2
+    jmp @@break
+
+@@sort_data:
+    push handle
+    call sort_record
     add sp, 2
     jmp @@break
 
 @@break:
+    jmp @@main_loop
+
+@@exit:
     push handle
     call fclose
     add sp, 2
 
-@@exit:
     _exit
 main endp
 
